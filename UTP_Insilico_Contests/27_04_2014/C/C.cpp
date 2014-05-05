0a4cb2a4b2c8927d9eb96421b92656e1fa588cea
Santiago Zubieta <santiago.zubieta@gmail.com>
2014-05-05 04:06:52 -0500

    Removed tab characters, replaced with spaces

--- UTP_Insilico_Contests/27_04_2014/C/C.cpp
+++ UTP_Insilico_Contests/27_04_2014/C/C.cpp
@@ -23,46 +23,46 @@
 using namespace std;
 
 int main(){
-	/*
-	 For a number up to 400, which is the amount of different pairs of prime numbers
-	 that can be added that will result in the given number? Like goldbach conjecture.
-	*/
-	int primes[78] = {  2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 
-		               59, 61, 67, 71, 73, 79, 83, 89, 97,101,103,107,109,113,127,131,
-		              137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,
-		              227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,
-		              313,317,331,337,347,349,353,359,367,373,379,383,389,397};
+    /*
+     For a number up to 400, which is the amount of different pairs of prime numbers
+     that can be added that will result in the given number? Like goldbach conjecture.
+    */
+    int primes[78] = {  2,  3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 
+                       59, 61, 67, 71, 73, 79, 83, 89, 97,101,103,107,109,113,127,131,
+                      137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,
+                      227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,
+                      313,317,331,337,347,349,353,359,367,373,379,383,389,397};
     // Precomputed list of primes up to 400, given in the problem statement. 
     int nums[401];
     for(int i = 0; i < 401; i++){
-    	nums[i] = 0;
+        nums[i] = 0;
     }
     for(int i = 0; i < 78; i++){
-    	int prime = primes[i];
-    	nums[prime] = prime;
+        int prime = primes[i];
+        nums[prime] = prime;
     }
     // The 'nums' array will contain the primes at their positions, others are just 0.
     int T, N;
     scanf("%d", &T);
     while(T--){
-    	scanf("%d", &N);
-    	int total = 0;
-    	for(int i = 1; i <= N/2; i++){
-    		// If the currently iterated number is not a prime, its array value is 0.
-    		if(nums[i] == 0) continue;
-    		int p1 = nums[i];
-    		// Now, the number at the current position is the first prime.
-    		int p2 = nums[N - p1];
-    		// And the other is at N - p1, which hopefully will be prime. 
-    		if(p1 + p2 == N){
-    			/*
-    			 If its not prime, then its 0, and this sum will not yield N.
-    			 But if there is a prime there, then p1 + p2 MUST give N since
-    			 p2 was found with N - p1.
-    			*/
-    			total++;
-    		}
-    	}
-    	printf("%d\n", total);
+        scanf("%d", &N);
+        int total = 0;
+        for(int i = 1; i <= N/2; i++){
+            // If the currently iterated number is not a prime, its array value is 0.
+            if(nums[i] == 0) continue;
+            int p1 = nums[i];
+            // Now, the number at the current position is the first prime.
+            int p2 = nums[N - p1];
+            // And the other is at N - p1, which hopefully will be prime. 
+            if(p1 + p2 == N){
+                /*
+                 If its not prime, then its 0, and this sum will not yield N.
+                 But if there is a prime there, then p1 + p2 MUST give N since
+                 p2 was found with N - p1.
+                */
+                total++;
+            }
+        }
+        printf("%d\n", total);
     }
 }
--- UTP_Insilico_Contests/27_04_2014/D/D.cpp
+++ UTP_Insilico_Contests/27_04_2014/D/D.cpp
@@ -23,49 +23,49 @@
 using namespace std;
 
 int main(){
-	/*
-	 Tobby is a dog who loves cute sequences.
-	 A cute sequence is one such that:
-	 >. It has size N (3 <= N <= 1000)
-	 >. Every number is unique
-	 >. Every number is 0 < Ai < 10^9
-	 >. Each Ai (0 < i < n) must satisfy:
-	      |Ai-1 - Ai| > |Ai - Ai+1|
-	 >. Tobby is such a nice dog :)
-	*/
+    /*
+     Tobby is a dog who loves cute sequences.
+     A cute sequence is one such that:
+     >. It has size N (3 <= N <= 1000)
+     >. Every number is unique
+     >. Every number is 0 < Ai < 10^9
+     >. Each Ai (0 < i < n) must satisfy:
+          |Ai-1 - Ai| > |Ai - Ai+1|
+     >. Tobby is such a nice dog :)
+    */
 
-	/*
-	 The solution is easy: Compute a list which
-	 starts at 1, then increments 1 to 2, then
-	 increments 2 to 4, then increments 3 to 7,
-	 etc. This way, the absolute difference be-
-	 tween a pair of numbers will be always bi-
-	 gger than the absolute difference of the
-	 next pair. So the numbers will go like:
+    /*
+     The solution is easy: Compute a list which
+     starts at 1, then increments 1 to 2, then
+     increments 2 to 4, then increments 3 to 7,
+     etc. This way, the absolute difference be-
+     tween a pair of numbers will be always bi-
+     gger than the absolute difference of the
+     next pair. So the numbers will go like:
 
-	 1 (+1) 2 (+2) 4 (+3) 7 (+4) 11 (+5) 16
+     1 (+1) 2 (+2) 4 (+3) 7 (+4) 11 (+5) 16
 
-	 Also, reverse the list, because the numbers
-	 have to be in a decreasing order.
+     Also, reverse the list, because the numbers
+     have to be in a decreasing order.
 
-	*/
-	int N;
-	vector<int> nums;
-	int cur = 1;
-	int inc = 1;
-	for(int i = 0; i < 1000; i++){
-		nums.push_back(cur);
-		cur += inc;
-		inc++;
-	}
-	reverse(nums.begin(), nums.end());
-	while(scanf("%d", &N) != EOF){
-		for(int i = 0; i < N; i++){
-			printf("%d", nums[i]);
-			if(i + 1 < N){
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}	
+    */
+    int N;
+    vector<int> nums;
+    int cur = 1;
+    int inc = 1;
+    for(int i = 0; i < 1000; i++){
+        nums.push_back(cur);
+        cur += inc;
+        inc++;
+    }
+    reverse(nums.begin(), nums.end());
+    while(scanf("%d", &N) != EOF){
+        for(int i = 0; i < N; i++){
+            printf("%d", nums[i]);
+            if(i + 1 < N){
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }    
 }
