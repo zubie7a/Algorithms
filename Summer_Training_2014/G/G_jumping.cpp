ff1d3d71361028f175dcb10981e0d96a3718b0be
zubieta <santiago.zubieta@gmail.com>
2014-06-12 23:27:48 -0500

    Added first batch of problems from Summer Training, 2014

--- Summer_Training_2014/G/G_jumping.cpp
+++ Summer_Training_2014/G/G_jumping.cpp
@@ -22,25 +22,25 @@
 
 using namespace std;
 
-int main(){
+int main() {
     int T;
     // Number of test cases
     int N;
     // Number of wall heights to read
     cin >> T;
-    for(int k = 1; k <= T; k++){
+    for(int k = 1; k <= T; k++) {
         cin >> N;
         int walls[N];
         int high = 0, low = 0;
         // Amount of high/low jumps between all walls.
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N; i++) {
             cin >> walls[i];
             if(i > 0){
-                if(walls[i] > walls[i - 1]){
+                if(walls[i] > walls[i - 1]) {
                 // If the current wall is taller than the previous one
                     high++;
                 }
-                if(walls[i] < walls[i - 1]){
+                if(walls[i] < walls[i - 1]) {
                 // If the current wall is lower than the previous one
                     low++;
                 }
