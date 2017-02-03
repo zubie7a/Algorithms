ae259383dfecd632104f415d6be227012135676d
Santiago Zubieta <santiago.zubieta@gmail.com>
2017-02-02 21:45:09 -0500

    Some problems from CTCI at HackerRank

---
+++ HackerRank/Cracking_The_Coding_Interview/Concepts/04_DP_Coin_Change.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/ctci-coin-change
+table = {}
+n, m = map(int, raw_input().split())
+coins = map(int, raw_input().split())
+ 
+# index: current coin being evaluated. It will only try that coin
+# once, and it will try with many multiples of that coin until
+# fitting the remaining amount.
+# n: the remaining amount.
+def compute(index, n):
+    # It added up, so this is a valid combination.
+    if n == 0:
+        return 1
+    # It exceeded the amount, so its not a valid combination.
+    if n < 0:
+        return 0
+    # The index exceeded capacity, so it reached the end and
+    # there was still money missing.
+    if index >= len(coins):
+        return 0
+    # If resu
+    if not table.get((index, n)):
+        table[(index, n)] = 0
+        # For all unused coins
+        coin, k = coins[index], 0
+        # Add multiples of coin until reaching n. 
+        while k*coin <= n:            
+            table[(index, n)] += compute(index + 1, n - k*coin)
+            k += 1
+    return table[(index, n)]
+ 
+print compute(0, n)
