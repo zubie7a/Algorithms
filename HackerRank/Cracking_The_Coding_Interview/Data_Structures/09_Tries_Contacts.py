e2a4449ad769834fc94263bdc02433ada0aba9cf
Santiago Zubieta <santiago.zubieta@gmail.com>
2017-02-03 21:01:51 -0500

    Trie implementation which passes 9/13 cases in HR, 4 fail because memory

--- HackerRank/Cracking_The_Coding_Interview/Data_Structures/09_Tries_Contacts.py
+++ HackerRank/Cracking_The_Coding_Interview/Data_Structures/09_Tries_Contacts.py
@@ -1,65 +1,48 @@
+from __future__ import print_function
 class Trie():
-    def __init__(self, char):
-        # The data value for this node.
-        self.char = char
-        # The current word so far.
-        self.word = ""
+    def __init__(self):
         # The children of this node.
         self.children = {}
-        # Whether this node is a complete word or not.
-        self.final = False
         # The amount of all children from this node downwards.
-        self.countChildren = 0
+        self.count = 0
         
     def add(self, string, index):
-        # If its the last position of the word being inserted.
-        if index + 1 == len(string):
-            # Mark the word as finalized.
-            self.final = True
+        # Increase the count of childrens for this character.
+        # At every insertion, when it goes through this node,
+        # it will increase the value, keeping track of all
+        # the children downwards.
+        self.count += 1
         # If there's still characters left to insert.
-        if index + 1 < len(string):
-            # If there's currently not a children with a given character.
-            if not self.children.get(string[index + 1]):
-                self.children[string[index + 1]] = Trie(string[index + 1])
-            # Increase the count of childrens for this character. At every
-            # insertion, when it goes through this node, it will increase
-            # the value, keeping track of all the children downwards.
-            self.countChildren += 1
-            # Call the add method on the children recursively from the
-            # next character onwards.
-            child = self.children[string[index + 1]]
+        if index < len(string):
+            # If there's currently not a children with a given char.
+            char = string[index]
+            if not self.children.get(char):
+                self.children[char] = Trie()
+            child = self.children[char]
+            # After creating/retrieving the children, call add on it
+            # and advance the index interator of the string.
             child.add(string, index + 1)
         
     def find(self, string, index):
-        # If the current node's character is the current query string
-        # character (it means it has matched so far).
-        if self.char == string[index]:
-            if index + 1 < len(string):
-                if self.children.get(string[index + 1]):
-                    child = self.children[string[index + 1]] 
-                    return child.find(string, index + 1)
-                else:
-                    return 0, False
+        # If there's still characters to match.
+        if index < len(string):
+            # If there's a node with the current character.
+            if self.children.get(string[index]):
+                child = self.children[string[index]]
+                # Go down recursively to it advancing the string index.
+                return child.find(string, index + 1)
             else:
-                completed = 0
-                if self.final == True:
-                    completed = 1
-                return self.countChildren + completed, True          
+                return 0
+        # When all characters in query have been matched.
         else:
-            return 0, False
+            return self.count
 
 n = int(raw_input())
-# A table of Tries for each possible starting letter.
-tries = {}
+trie = Trie()
 for _ in xrange(n):
     op, word = raw_input().split()
     if op == "add":
-        if not tries.get(word[0]):
-            tries[word[0]] = Trie(word[0])
-        tries[word[0]].add(word, 0)
+        trie.add(word, 0)
     if op == "find":
-        if not tries.get(word[0]):
-            print 0
-        else:
-            count, _ = tries[word[0]].find(word, 0)
-            print count  
+        count = trie.find(word, 0)
+        print(count)
