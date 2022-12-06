3af8fb9f933b64b5ec49523af7b21129ef7972c0
Santiago Zubieta <652018+zubie7a@users.noreply.github.com>
2022-12-06 08:21:39 +0100

    Add first day of 2022 Advent of Code in Kotlin

---
+++ Advent_of_Code/2022/Kotlin/src/Day01.kt
@@ -0,0 +1,47 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+
+        var currentElfWeight = 0
+        var listOfElvesWeights = mutableListOf<Int>()
+
+        input.forEach { row ->
+            if (row.isNotEmpty()) {
+                currentElfWeight += row.toInt()
+            } else {
+                listOfElvesWeights.add(currentElfWeight)
+                currentElfWeight = 0
+            }
+        }
+
+        return listOfElvesWeights.max()
+    }
+
+    fun part2(input: List<String>): Int {
+
+        var currentElfWeight = 0
+        var listOfElvesWeights = mutableListOf<Int>()
+
+        input.forEach { row ->
+            if (row.isNotEmpty()) {
+                currentElfWeight += row.toInt()
+            } else {
+                listOfElvesWeights.add(currentElfWeight)
+                currentElfWeight = 0
+            }
+        }
+
+        // return listOfElvesWeights.sorted().reversed().subList(0, 3).sum()
+        // return listOfElvesWeights.sorted().takeLast(3).sum()
+        return listOfElvesWeights.sortedDescending().take(3).sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    println(part1(testInput))
+    println(part2(testInput))
+
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input))
+}
